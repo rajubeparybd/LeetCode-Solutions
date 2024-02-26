0685dc497e61e4ea912b41891e22e7203b6257d6
Raju Bepary <raju174829@gmail.com>
2024-02-26 10:46:20 +0600

    Run Time: 580ms - LeetCode

---
+++ 1211 - Queries Quality and Percentage/QueriesQuality.sql
@@ -0,0 +1,6 @@
+SELECT query_name,
+       ROUND(AVG(rating / position), 2)                 quality,
+       ROUND(SUM(IF(rating < 3, 100, 0)) / COUNT(*), 2) poor_query_percentage
+from Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name;
