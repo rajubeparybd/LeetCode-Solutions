6ef4065f1d19bae6885de42df90b516df859162a
Raju Bepary <raju174829@gmail.com>
2024-03-20 15:40:01 +0600

    Solution 2 | Classes more than 5 students

--- 596. Classes More Than 5 Students/Classes.sql
+++ 596. Classes More Than 5 Students/Classes.sql
@@ -2,3 +2,8 @@ SELECT class
 FROM Courses
 GROUP BY class
 HAVING COUNT(student) >= 5;
+
+-- SOLUTION 2
+SELECT DISTINCT class
+FROM Courses
+WHERE (SELECT COUNT(*) FROM Courses As c WHERE c.class = Courses.class) >= 5
